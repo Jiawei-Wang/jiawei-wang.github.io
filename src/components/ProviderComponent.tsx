ea84ba9966ffc0bbccb4b0d7c2e2e9697dce5777
Jiawei Wang <wangjiawei_med@hotmail.com>
2025-08-17 15:21:25 -0400

    change content

--- src/components/ProviderComponent.tsx
+++ src/components/ProviderComponent.tsx
@@ -4,11 +4,11 @@ import './ProviderComponent.css';
 
 // Import images
 import meImg from '../images/me.jpg';
-// import deepSeaImg from '../images/the-deep-sea.png';
+import underConstruction from '../images/under-construction.png'
 
 // Import contact icons
-import twitterIcon from '../images/twitter.svg';
-import mailIcon from '../images/mail.svg';
+import linkedinIcon from '../images/linkedin.png';
+import githubIssueIcon from '../images/github.svg';
 
 const Provider: React.FC = () => {
   return (
@@ -26,27 +26,50 @@ const Provider: React.FC = () => {
 
       {/* Projects */}
       <div className="Section">
-        <h2 className="SectionHeader">Some things I made</h2>
+        <h2 className="SectionHeader">This website is under construction</h2>
 
-        {/* example */}
-        {/* <a href="https://neal.fun/deep-sea/" target="_blank" rel="noopener" className="ProjectWrapper">
-          <img alt="The Deep Sea" src={deepSeaImg} className="ProjectThumbnail" />
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ/" target="_blank" rel="noopener" className="ProjectWrapper">
+          <img alt="The Deep Sea" src={underConstruction} className="ProjectThumbnail" />
           <div className="ProjectInfo">
-            <div className="ProjectTitle">The Deep Sea</div>
-            <div className="ProjectDesc">A page that lets you scroll down the ocean</div>
+            <div className="ProjectTitle">The Foo Bar</div>
+            <div className="ProjectDesc">A page that lets you foo the bar</div>
           </div>
-        </a> */}
+        </a>
+
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ/" target="_blank" rel="noopener" className="ProjectWrapper">
+          <img alt="The Deep Sea" src={underConstruction} className="ProjectThumbnail" />
+          <div className="ProjectInfo">
+            <div className="ProjectTitle">The Foo Bar</div>
+            <div className="ProjectDesc">A page that lets you foo the bar</div>
+          </div>
+        </a>
+
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ/" target="_blank" rel="noopener" className="ProjectWrapper">
+          <img alt="The Deep Sea" src={underConstruction} className="ProjectThumbnail" />
+          <div className="ProjectInfo">
+            <div className="ProjectTitle">The Foo Bar</div>
+            <div className="ProjectDesc">A page that lets you foo the bar</div>
+          </div>
+        </a>
+
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ/" target="_blank" rel="noopener" className="ProjectWrapper">
+          <img alt="The Deep Sea" src={underConstruction} className="ProjectThumbnail" />
+          <div className="ProjectInfo">
+            <div className="ProjectTitle">The Foo Bar</div>
+            <div className="ProjectDesc">A page that lets you foo the bar</div>
+          </div>
+        </a>
       </div>
 
       {/* Contact */}
       <div className="Section">
         <div className="LogoHeader">Do you have something in mind? Get in touch!</div>
         <div className="GetInTouch">
-          <a target="_blank" rel="noopener" href="https://twitter.com/nealagarwal" className="ButtonLink twitter">
-            <img alt="Twitter Icon" src={twitterIcon} className="ButtonIcon" /> Twitter
+          <a target="_blank" rel="noopener" href="https://www.linkedin.com/in/hire-jiawei-wang/" className="ButtonLink linkedin">
+            <img alt="LinkedIn Icon" src={linkedinIcon} className="ButtonIcon" /> LinkedIn
           </a>
-          <a target="_blank" rel="noopener" href="mailto:hi@neal.fun" className="ButtonLink email">
-            <img alt="Email Icon" src={mailIcon} className="ButtonIcon" /> Email
+          <a target="_blank" rel="noopener" href="https://github.com/Jiawei-Wang/jiawei-wang.github.io/issues/new" className="ButtonLink issue">
+            <img alt="Issue Icon" src={githubIssueIcon} className="ButtonIcon" /> Github Issue
           </a>
         </div>
       </div>
