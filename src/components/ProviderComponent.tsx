85eda04fc24f8640b79bb1755f74099435ecce10
Jiawei Wang <wangjiawei_med@hotmail.com>
2025-08-17 15:25:55 -0400

    format

--- src/components/ProviderComponent.tsx
+++ src/components/ProviderComponent.tsx
@@ -1,14 +1,14 @@
 // ProviderComponent.tsx
-import React from 'react';
-import './ProviderComponent.css';
+import React from "react";
+import "./ProviderComponent.css";
 
 // Import images
-import meImg from '../images/me.jpg';
-import underConstruction from '../images/under-construction.png'
+import meImg from "../images/me.jpg";
+import underConstruction from "../images/under-construction.png";
 
 // Import contact icons
-import linkedinIcon from '../images/linkedin.png';
-import githubIssueIcon from '../images/github.svg';
+import linkedinIcon from "../images/linkedin.png";
+import githubIssueIcon from "../images/github.svg";
 
 const Provider: React.FC = () => {
   return (
@@ -20,7 +20,8 @@ const Provider: React.FC = () => {
         </div>
         <h1 className="HeaderTitle">Hi, I'm Jiawei!</h1>
         <div className="Tagline">
-          I'm an enterprise software engineer with focus on ML/Data/Backend, and this is my Github Page. Chica is my rescue dog and pair programmer.
+          I'm an enterprise software engineer focused on ML/Data/Backend, and
+          this is my GitHub Page. Chica is my rescue dog and pair programmer.
         </div>
       </div>
 
@@ -28,32 +29,68 @@ const Provider: React.FC = () => {
       <div className="Section">
         <h2 className="SectionHeader">This website is under construction</h2>
 
-        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ/" target="_blank" rel="noopener" className="ProjectWrapper">
-          <img alt="The Deep Sea" src={underConstruction} className="ProjectThumbnail" />
+        <a
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ/"
+          target="_blank"
+          rel="noopener"
+          className="ProjectWrapper"
+        >
+          <img
+            alt="The Deep Sea"
+            src={underConstruction}
+            className="ProjectThumbnail"
+          />
           <div className="ProjectInfo">
             <div className="ProjectTitle">The Foo Bar</div>
             <div className="ProjectDesc">A page that lets you foo the bar</div>
           </div>
         </a>
 
-        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ/" target="_blank" rel="noopener" className="ProjectWrapper">
-          <img alt="The Deep Sea" src={underConstruction} className="ProjectThumbnail" />
+        <a
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ/"
+          target="_blank"
+          rel="noopener"
+          className="ProjectWrapper"
+        >
+          <img
+            alt="The Deep Sea"
+            src={underConstruction}
+            className="ProjectThumbnail"
+          />
           <div className="ProjectInfo">
             <div className="ProjectTitle">The Foo Bar</div>
             <div className="ProjectDesc">A page that lets you foo the bar</div>
           </div>
         </a>
 
-        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ/" target="_blank" rel="noopener" className="ProjectWrapper">
-          <img alt="The Deep Sea" src={underConstruction} className="ProjectThumbnail" />
+        <a
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ/"
+          target="_blank"
+          rel="noopener"
+          className="ProjectWrapper"
+        >
+          <img
+            alt="The Deep Sea"
+            src={underConstruction}
+            className="ProjectThumbnail"
+          />
           <div className="ProjectInfo">
             <div className="ProjectTitle">The Foo Bar</div>
             <div className="ProjectDesc">A page that lets you foo the bar</div>
           </div>
         </a>
 
-        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ/" target="_blank" rel="noopener" className="ProjectWrapper">
-          <img alt="The Deep Sea" src={underConstruction} className="ProjectThumbnail" />
+        <a
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ/"
+          target="_blank"
+          rel="noopener"
+          className="ProjectWrapper"
+        >
+          <img
+            alt="The Deep Sea"
+            src={underConstruction}
+            className="ProjectThumbnail"
+          />
           <div className="ProjectInfo">
             <div className="ProjectTitle">The Foo Bar</div>
             <div className="ProjectDesc">A page that lets you foo the bar</div>
@@ -63,13 +100,35 @@ const Provider: React.FC = () => {
 
       {/* Contact */}
       <div className="Section">
-        <div className="LogoHeader">Do you have something in mind? Get in touch!</div>
+        <div className="LogoHeader">
+          Do you have something in mind? Get in touch!
+        </div>
         <div className="GetInTouch">
-          <a target="_blank" rel="noopener" href="https://www.linkedin.com/in/hire-jiawei-wang/" className="ButtonLink linkedin">
-            <img alt="LinkedIn Icon" src={linkedinIcon} className="ButtonIcon" /> LinkedIn
+          <a
+            target="_blank"
+            rel="noopener"
+            href="https://www.linkedin.com/in/hire-jiawei-wang/"
+            className="ButtonLink linkedin"
+          >
+            <img
+              alt="LinkedIn Icon"
+              src={linkedinIcon}
+              className="ButtonIcon"
+            />{" "}
+            LinkedIn
           </a>
-          <a target="_blank" rel="noopener" href="https://github.com/Jiawei-Wang/jiawei-wang.github.io/issues/new" className="ButtonLink issue">
-            <img alt="Issue Icon" src={githubIssueIcon} className="ButtonIcon" /> Github Issue
+          <a
+            target="_blank"
+            rel="noopener"
+            href="https://github.com/Jiawei-Wang/jiawei-wang.github.io/issues/new"
+            className="ButtonLink issue"
+          >
+            <img
+              alt="Issue Icon"
+              src={githubIssueIcon}
+              className="ButtonIcon"
+            />{" "}
+            Github Issue
           </a>
         </div>
       </div>
